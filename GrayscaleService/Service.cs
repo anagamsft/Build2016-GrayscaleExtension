c389651f82071c5bee03cc3b695b8e6654778c30
Anir Agarwal <anaga@microsoft.com>
2016-03-23 18:20:29 -0700

    Committing App Service - no idea how to convert to grayscale.

---
+++ GrayscaleService/Service.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace GrayscaleService
+{
+    public sealed class Service : IBackgroundTask
+    {
+        private BackgroundTaskDeferral backgroundTaskDeferral;
+        private AppServiceConnection appServiceconnection;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            this.backgroundTaskDeferral = taskInstance.GetDeferral(); // Get a deferral so that the service isn&#39;t terminated.
+            taskInstance.Canceled += OnTaskCanceled; // Associate a cancellation handler with the background task.
+
+            // Retrieve the app service connection and set up a listener for incoming app service requests.
+            var details = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            appServiceconnection = details.AppServiceConnection;
+            appServiceconnection.RequestReceived += OnRequestReceived;
+        }
+
+        private async void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            // Get a deferral because we use an awaitable API below to respond to the message
+            // and we don&#39;t want this call to get cancelled while we are waiting.
+            var messageDeferral = args.GetDeferral();
+
+            ValueSet message = args.Request.Message;
+            ValueSet returnData = new ValueSet();
+
+            string command = message["Command"] as string;
+
+            switch (command)
+            {
+                case "Grayscale":
+                    {
+                        string imageString = message["ImageString"] as string;
+                        returnData.Add("ImageString", imageString);
+
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+
+            await args.Request.SendResponseAsync(returnData); // Return the data to the caller.
+            messageDeferral.Complete(); // Complete the deferral so that the platform knows that we&#39;re done responding to the app service call.
+        }
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            if (this.backgroundTaskDeferral != null)
+            {
+                // Complete the service deferral.
+                this.backgroundTaskDeferral.Complete();
+            }
+        }
+    }
+}
